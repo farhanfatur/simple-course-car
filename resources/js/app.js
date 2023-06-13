c7d2d46a3048b0debfeb7089c5275cc3f8b697ff
farhan <farhan@kanosolution.com>
2023-06-13 22:25:11 +0700

    First Changes

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+require('./bootstrap');
+import { 
+    Datepicker, 
+    Collapse,
+    Dropdown, 
+    Input, 
+    initTE } from "tw-elements";
+initTE({ Collapse, Dropdown, Datepicker, Input });
